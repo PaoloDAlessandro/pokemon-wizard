fdd6c40a6db1e275972a4e2d25a553007965c8e7
PaoloDAlessandro <paolo.dalessandro02@gmail.com>
2024-10-14 23:59:31 +0200

    fix: pokemon selection layout style

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import { Typography } from "./components/Typography";
 import Wizard from "./components/Wizard"; // Import the Wizard component
+import WizardProgress from "./components/WizardProgress";
 
 function App() {
+  const [step, setStep] = useState(1);
   return (
-    <div className="App bg-gray-100 h-[100dvh] flex flex-col justify-between items-center">
-      <header className="App-header mt-3">
-        <Typography as="h1" variant="h2" className="text-center text-4xl font-bold pokemon-font px-3">
-          Pokémon Team Builder
-        </Typography>
+    <div className="App bg-gray-100 min-h-[100dvh] flex flex-col">
+      <header className="sticky top-0 z-10 bg-gray-100">
+        <div className="App-header py-3 w-11/12 lg:w-full lg:max-w-[96rem] mx-auto space-y-5">
+          <Typography as="h1" variant="h2" className="text-center text-4xl font-bold pokemon-font">
+            Pokémon Team Builder
+          </Typography>
+          <WizardProgress currentStep={step} />
+        </div>
       </header>
-      <main className="mt-8 w-full h-full">
-        <Wizard />
+      <main className="flex-grow w-full mt-2 lg:mt-5 px-4">
+        <Wizard step={step} setStep={setStep} />
       </main>
     </div>
   );
--- src/components/Wizard.tsx
+++ src/components/Wizard.tsx
@@ -5,8 +5,12 @@ import PokemonSelection from "./PokemonSelection";
 import TrainerDetails from "./TrainerDetails";
 import WizardProgress from "./WizardProgress";
 
-const Wizard: React.FC = () => {
-  const [step, setStep] = useState(1);
+interface WizardProps {
+  step: number;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const Wizard: React.FC<WizardProps> = ({ step, setStep }) => {
   const [trainerInfo, setTrainerInfo] = useState<TrainerInfo | null>(null);
   const [team, setTeam] = useState<PokemonTeam>([]);
   const [opponentTeam, setOpponentTeam] = useState<PokemonTeam>([]);
@@ -16,8 +20,6 @@ const Wizard: React.FC = () => {
 
   return (
     <div className="w-11/12 lg:w-full lg:max-w-[96rem] h-full mx-auto">
-      <WizardProgress currentStep={step} />
-
       {step === 1 && <TrainerDetails setTrainerInfo={setTrainerInfo} nextStep={nextStep} />}
       {step === 2 && <PokemonSelection trainerInfo={trainerInfo} setTeam={setTeam} />}
       {step === 3 && <OpponentSelection team={team} setOpponentTeam={setOpponentTeam} />}
